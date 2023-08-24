e464c4603ddbd7c7b245cc4eaf8f2f5127bd998b
eyyup demir <ludwigsburg100@gmail.com>
2023-08-24 11:52:16 +0200

    24,08,23

---
+++ src/day41_exceptions/deneme.java
@@ -0,0 +1,14 @@
+package day41_exceptions;
+
+public class deneme {
+    public static void main(String[] args) {
+
+        for (int i = 2147482646; i >0 ; i++) {
+            System.out.println(i);
+
+        }
+
+        int a =9;
+
+
+    }}
